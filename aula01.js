24e8b38fa26bec0bf6c76c510c7a34e97908e57a
unknown <ezioweb@hotmail.com>
2021-08-25 20:22:26 -0300

    1o Checkpoint de programação

---
+++ aula01.js
@@ -0,0 +1,91 @@
+// If Else
+
+/* function obterUmCargoPelo(salario){
+    let cargo;
+    if(salario < 2500.00){
+        cargo = "Junior";
+    }
+    else if(salario < 5000.00){
+        cargo = "Pleno";
+    }
+    else if(salario < 10000.00){
+        cargo = "Senior";
+    }
+    console.log(cargo);
+    return cargo;
+} */
+
+// if ternário
+/* function obterUmCargoPelo(salario){
+    let cargo = salario < 2500.00 ? "junir" : salario < 5000.00 ? "Pleno" : salario < 10000.00 ? "Senior" : "";
+    console.log(cargo);
+    return cargo;
+}
+
+obterUmCargoPelo(11000) */
+
+// switch
+function obterSalarioMaximoPeloCargo(cargo)
+let salarioMaximo = 0.0;
+switch(cargo){
+    case "Junior"
+    salarioMaximo = 2500.0;
+    break;
+    case "Pleno"
+    salarioMaximo = 5000.0;
+    break;
+    case "Senior"
+    salarioMaximo = 10000.0;
+    break;
+    defaul:
+    salarioMaximo = 10000000.0;
+console.log(salarioMaximo);
+return salarioMaximo
+}
+let cargo = "Senior"
+obterSalarioMaximoPeloCargo(cargo)
+
+function obterUmCargoPeloSalario(salario) {
+    let cargo;
+    
+    if(salario < 2500.00){
+      cargo = "Junior";
+    } else if(salario < 5000.0){
+      cargo = "Pleno";
+    } else if(salario < 10000.0){
+      cargo = "Senior";
+    } else {
+      cargo = "";
+    }
+    console.log(cargo);
+    return cargo;
+  }
+  
+  function obterSalarioMaximoPeloCargo(cargo){
+    let salarioMaximo = 0.0;
+    switch(cargo){
+      case "Junior":
+        salarioMaximo = 2500.0;
+        break;
+      case "Pleno":
+        salarioMaximo = 5000.0;
+        break;
+      case "Senior":
+        salarioMaximo = 10000.0;
+        break;
+      case "Executivo":
+        salarioMaximo = 20000.0;
+        break;
+      default:
+        salarioMaximo = 10000000.0;
+    }
+  
+    console.log(salarioMaximo);
+    return salarioMaximo;
+  
+  }
+  
+  let cargo = "Executivo";
+  
+  obterSalarioMaximoPeloCargo(cargo);
+  
---
+++ aula09.js
@@ -0,0 +1,104 @@
+/* Grupo Sala 01
+
+Allan Cardoso
+Ezio Roberto Lorenzetti 
+Gabriela Gomes de Lima
+Lucas Alcântara
+Lucas <ota Barbosa da Silva
+*/
+
+/*  1 - Pipoca – 10 segundos (padrão);
+    2 - Macarrão – 8 segundos (padrão);
+    3 - Carne – 15 segundos (padrão);
+    4 - Feijão – 12 segundos (padrão);
+    5 - Brigadeiro – 8 segundos (padrão);  */
+
+function microOndas (prato, tempo){
+    let tempoDoPrato = tempo
+    switch (prato){
+        case 1:  //pipoca 10 seg.
+        if (tempoDoPrato < 10){
+            console.log("Tempo insuficiente");
+        }
+        else if (tempoDoPrato >= 20 && tempoDoPrato < 30){
+            console.log("Prato queimou");
+        }
+        else if(tempoDoPrato >= 30){
+            console.log("Kabumm");
+        }
+        else {
+            console.log("Prato Pronto");
+        }
+        break;
+
+
+        case 2:  //Macarrão 8 seg.
+        if (tempoDoPrato < 8){
+            console.log("Tempo insuficiente");
+        }
+        else if (tempoDoPrato >= 16 && tempoDoPrato < 24){
+            console.log("Prato queimou");
+        }
+        else if(tempoDoPrato >= 24){
+            console.log("Kabumm");
+        }
+        else {
+            console.log("Prato Pronto");
+        }
+        break;
+
+
+        case 3:  //carne 15 seg.
+        if (tempoDoPrato < 15){
+            console.log("Tempo insuficiente");
+        }
+        else if (tempoDoPrato >= 30 && tempoDoPrato < 45){
+            console.log("Prato queimou");
+        }
+        else if(tempoDoPrato >= 45){
+            console.log("Kabumm");
+        }
+        else {
+            console.log("Prato Pronto");
+        }
+        break;
+
+
+        case 4:  //Feijão 12 seg.
+        if (tempoDoPrato < 12){
+            console.log("Tempo insuficiente");
+        }
+        else if (tempoDoPrato >= 24 && tempoDoPrato < 36){
+            console.log("Prato queimou");
+        }
+        else if(tempoDoPrato >= 36){
+            console.log("Kabumm");
+        }
+        else {
+            console.log("Prato Pronto");
+        }
+        break;
+
+
+        case 5:  //Brigadeiro 8 seg.
+        if (tempoDoPrato < 8){
+            console.log("Tempo insuficiente");
+        }
+        else if (tempoDoPrato >= 16 && tempoDoPrato < 24){
+            console.log("Prato queimou");
+        }
+        else if(tempoDoPrato >= 24){
+            console.log("Kabumm");
+        }
+        else {
+            console.log("Prato Pronto");
+        }
+        break;
+
+        default: 
+            console.log("Prato imexistente")
+        }
+}
+
+
+microOndas (5, 24)
